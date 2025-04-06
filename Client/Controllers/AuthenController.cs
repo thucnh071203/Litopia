6f48abfa5ab6f337abf5cf811e9defb0b65faa96
THUC <nht.071203@gmail.com>
2025-04-06 21:30:31 +0700

    add templete

---
+++ Client/Controllers/AuthenController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Client.Controllers
+{
+    public class AuthenController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
